9329102bef94cfcc4eb1a21fcf15431d7886ceeb
Matheus Diniz <70189571+DinizMaths@users.noreply.github.com>
2020-09-18 13:58:04 -0300

    Add files via upload

---
+++ Edition_2/web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Landing from './pages/Landing'; Não precida colocar o /nomeDoArquivo pq ele é index, então vai automaticamente pra ele
+import Routes from './routes';
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
